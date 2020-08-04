0eb7cb3b151b95381dee3a8c21cb0ab75c40012e
riventus <riventus@aksaramaya.com>
2020-08-04 17:42:41 +0700

    update

---
+++ build/precache-manifest.74fcd12b89bb34da6dfdb0b1cdd36704.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6a4bb4b80860e2a0c4ded7aceeb56",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "869d02ca6e76f94a9f13",
+    "url": "/build/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "08244c485565a15d432f",
+    "url": "/build/static/css/main.667fad90.chunk.css"
+  },
+  {
+    "revision": "869d02ca6e76f94a9f13",
+    "url": "/build/static/js/2.4e6ac25b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/build/static/js/2.4e6ac25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08244c485565a15d432f",
+    "url": "/build/static/js/main.6b219edc.chunk.js"
+  },
+  {
+    "revision": "1b876c0d4adb369532c0",
+    "url": "/build/static/js/runtime-main.babb2ca4.js"
+  },
+  {
+    "revision": "2ba164ff263279ae810cfc969cf1ab93",
+    "url": "/build/static/media/bg-mobile-fallback.2ba164ff.jpg"
+  },
+  {
+    "revision": "6e3daaaa1b97bc16e6280e80b26d923f",
+    "url": "/build/static/media/bg.6e3daaaa.mp4"
+  }
+]);
