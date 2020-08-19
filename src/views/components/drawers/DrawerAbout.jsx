f7d7edfc675961fae32e175928f36d74b95ab7db
riventus <riventus@aksaramaya.com>
2020-08-19 11:44:47 +0700

    update note skets

--- src/views/components/drawers/DrawerAbout.jsx
+++ src/views/components/drawers/DrawerAbout.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { Layout } from "antd";
+import { Carousel } from "antd";
 import DrawerContainer from "./DrawerContainer";
-
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 export default (props) => {
   return(<DrawerContainer {...props}>
-    ASDASKDMLAMD
+    <div style={{backgroundColor:'tomato'}}>
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
   </DrawerContainer>)
 };
