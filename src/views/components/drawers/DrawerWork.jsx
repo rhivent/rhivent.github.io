4e6bd9051b9fe9772bd46f3690f7c2d7af10c896
rhivent <zzzzjrjr@gmail.com>
2020-08-25 10:11:29 +0700

    update drawer work

--- src/views/components/drawers/DrawerWork.jsx
+++ src/views/components/drawers/DrawerWork.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import DrawerContainer from "./DrawerContainer";
 
+// const url = 'https://www.jagoanhosting.com/blog/cara-berpikir-kreatif/';
+const url = 'https://riventus-smart-app.vercel.app/';
+
 export default (props) => {
   return(<DrawerContainer {...props}>
     <div>
     <iframe 
       style={{height:'90vh'}}
       width="100%" 
-      src="https://dev.to/g33konaut/understanding-cors-aaf" frameBorder="0">
+      src={url} frameBorder="0">
     </iframe>
-
     </div>
   </DrawerContainer>)
 };
